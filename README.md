490110b63c5e20565bcaaa07008ed423cdfb55ac
Efim Manevich <efimmanevich@gmail.com>
2020-04-04 12:10:11 +0300

    Версия 2.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # websocketserver
 Push server on Node.js
+:8080 chat
+:8080/admin  - admin info
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const https = require('https');
 const url = require('url');
 const WebSocket = require('ws');
 
@@ -11,43 +12,74 @@ app.set('view engine', 'ejs');
 
 
 
+
 var listConnect = [];
 var connectionIDCounter = 0;
+
 var port = process.env.PORT || 8080;
 
 
-function sendObj(message)
-{
+function sendObj(message) {
     var to = message.to;
     if (!to)
         return;
     message.to = '';
     var strmes = JSON.stringify(message);
+    var from = message.from;
+
     var adr = to.split(';');
-    adr.forEach(function(item, i, a){
-         sendMessage(item, strmes);   
+
+    adr.forEach(function (item, i, a) {
+        sendMessage(item, strmes, from);
     });
 }
 
 
-function sendMessage(to, message)
-{
-    for (var i=0; i < listConnect.length; i++)
-    {
+function sendMessage(to, message, from) {
+    for (var i = 0; i < listConnect.length; i++) {
+        if (i < 0)
+            break;
         if (to == 'all' || to == listConnect[i].info.UserID || to == listConnect[i].info.UserName || to == listConnect[i].info.UserGroup)
-            listConnect[i].send(message);
-    }
-    
-}
+        {
+            try {
+                listConnect[i].send(message);
+            }
+            catch (err) {
+                listConnect.splice(i, 1);
+                i--;
+            }
+            
+            if (to == listConnect[i].info.UserID || to == listConnect[i].info.UserName)
+            {
+                
+                for (var j = 0; j < listConnect.length; j++)
+                {
+                    if (listConnect[j].info.UserID == from)
+                    try {
+                        listConnect[j].send(message);
+                    }
+                    catch (err) {
+                        
+                    }
 
-app.use(function (req, res) {
-  res.render('index', {listConnect : listConnect});
+                }
+            }
+            
+
+        }    
+    }
+
+}
+app.use(express.static('assets'));
+app.get('/admin', function (req, res) {
+    res.render('index', { listConnect: listConnect });
 });
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server: server,
-                                   clientNoContextTakeover: true,
-                                   serverNoContextTakeover: true
+const wss = new WebSocket.Server({
+    server: server,
+    clientNoContextTakeover: true,
+    serverNoContextTakeover: true
 });
 
 
@@ -57,104 +89,107 @@ wss.on('connection', function connection(ws, req) {
     //const location = url.parse(req.url, true);
     // You might use location.query.access_token to authenticate or share sessions
     // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
     var SID = connectionIDCounter++;
     var Start = new Date();
-    ws.info = {SID:SID, Start: Start};
+    ws.info = { SID: SID, Start: Start };
     listConnect.push(ws);
     //console.log(listConnect.length.toString());
 
-  ws.on('message', function incoming(message) {
-    //console.log('received: %s', message);
-      try
-      {
-      objMes = JSON.parse(message);
-      var mtype = objMes.type;
-      if (mtype == 'echo')
-      {
-          ws.send(message);
-          return;
-      }
-      
-      if (mtype == 'auth')
-      {
-         if (objMes.UserID)
-             ws.info.UserID = objMes.UserID;
-         if (objMes.UserName)
-             ws.info.UserName = objMes.UserName;
-         if (objMes.UserGroup)
-             ws.info.UserGroup = objMes.UserGroup;
-         return;
-      }
-      
-      if (mtype == 'message')
-      {
-          var to = objMes.to;
-          if (!to)
-          {
-              var errorto = 
-                        {
-                            type : "error",
-                            fromName : "error",
-                            message : "Empty field 'to'",
-                            source : message
-                        };
-              try
-              {
-              ws.send (JSON.stringify(errorto));
-              }
-              catch (ex)
-              {;}
-              return;
-          }
-           
-          sendObj(objMes);
-          return;
-      }
-      
-      var ansve = 
-                {
-                    type : "error",
-                    fromName : "error",
-                    message : "Unknown type",
-                    source : message
-                };
-      try {                
-            ws.send(JSON.stringify(ansve));
-          }
-           catch (ex)
-           {;}
-      }
-      catch(e)
-      {
-          var ansve =
-                {
-                    type : "error",
-                    fromName : "error",
-                    message : e.message,
-                    source : message
-                };
-           try {     
-           ws.send(JSON.stringify(ansve));     
-           }
-           catch (ex)
-           {;}
-          
-      }
-      
-  });
+    ws.on('error', () => console.log('errored'));
 
-  ws.on('close', function (code, reason){
-    var idx = listConnect.indexOf(ws);
-    if (idx != -1) {
-        listConnect.splice(idx, 1);
-    }
-    //console.log('Disconnect ' + ws.info.SID.toString());
-    //console.log(listConnect.length.toString());
-  });
-  
-  
+    ws.on('message', function incoming(message) {
+        //console.log('received: %s', message);
+        try {
+            objMes = JSON.parse(message);
+            var mtype = objMes.type;
+            if (mtype == 'echo') {
+                ws.send(message);
+                return;
+            }
+
+            if (mtype == 'auth') {
+                if (objMes.UserID)
+                    ws.info.UserID = objMes.UserID;
+                if (objMes.UserName)
+                    ws.info.UserName = objMes.UserName;
+                if (objMes.UserGroup)
+                    ws.info.UserGroup = objMes.UserGroup;
+                return;
+            }
+
+            if (mtype == 'message') {
+                var to = objMes.to;
+                if (!to) {
+                    var errorto =
+                    {
+                        type: "error",
+                        fromName: "error",
+                        message: "Empty field 'to'",
+                        source: message
+                    };
+                    try {
+                        ws.send(JSON.stringify(errorto));
+                    }
+                    catch (ex) { ; }
+                    return;
+                }
+                //Отправляем эхо
+                //ws.send(message);
+                sendObj(objMes);
+                return;
+            }
+            //Неизвестный тип сообщения    
+            var ansve =
+            {
+                type: "error",
+                fromName: "error",
+                message: "Unknown type",
+                source: message
+            };
+            try {
+                ws.send(JSON.stringify(ansve));
+            }
+            catch (ex) { ; }
+        }
+        catch (e) {
+            var ansve =
+            {
+                type: "error",
+                fromName: "error",
+                message: e.message,
+                source: message
+            };
+            try {
+                ws.send(JSON.stringify(ansve));
+            }
+            catch (ex) { ; }
+
+        }
+
+    });
+
+    ws.on('close', function (code, reason) {
+        var idx = listConnect.indexOf(ws);
+        if (idx != -1) {
+            listConnect.splice(idx, 1);
+        }
+        //console.log('Disconnect ' + ws.info.SID.toString());
+        //console.log(listConnect.length.toString());
+    });
+
+
 });
 
+
+setInterval(function timeout() {
+    try {
+        sendMessage('all', '', '');
+    }
+    catch (err) { ; }
+}, 30000);
+
+
 server.listen(port, function listening() {
-  console.log('Start server:' + port.toString());
+    console.log('Start server:' + port.toString());
 });
